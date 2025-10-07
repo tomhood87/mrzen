3b58b9b1062ce47c4161c68a0f3b7b95ae27df61
Tom Hood <tom.hood@mrzen.com>
2025-10-07 23:39:01 +0100

    Test layout.

---
+++ nuxt-layer/layouts/default.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mz-layout-default mrzen-default-layout">
+    <!-- Page content -->
+    <main class="flex-1 container mx-auto p-6">
+      <slot />
+    </main>
+  </div>
+</template>
