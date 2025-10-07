497ca29e195dec5f44acd0398e45a4fa88919107
Tom Hood <tom.hood@mrzen.com>
2025-10-07 17:48:01 +0100

    Remove config index for testing.

--- src/plugin.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import ClientDefaultLayout from './layouts/default.vue'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.component('NuxtLayout', ClientDefaultLayout)
-})
