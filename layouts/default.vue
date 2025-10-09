eda88c67ae96400b2f4e23d84e3898d0e75e6317
Tom Hood <tom.hood@mrzen.com>
2025-10-09 15:23:34 +0100

    Add header and footer.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,15 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from '~/layouts/header/index.vue'
+import Footer from '~/layouts/footer/index.vue'
+</script>
 
 <template>
+  <Header />
   <div class="mz-layout-default mrzen-default-layout">
     <!-- Page content -->
     <main class="flex-1 container mx-auto p-6">
       <slot />
     </main>
   </div>
+  <Footer />
 </template>
