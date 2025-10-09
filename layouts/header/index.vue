c9d585454e8fa0aad1ebcf91ba89e5ae7d2da1e4
Tom Hood <tom.hood@mrzen.com>
2025-10-09 16:38:25 +0100

    Add site logo.

--- layouts/header/index.vue
+++ layouts/header/index.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
 const { data:menu } = await useFetch('/api/menu')
+const { data: settings } = await useAsyncData('site-settings', () =>
+  $fetch('/api/site-settings')
+)
 </script>
 
 <template>
   <header id="header-layout-one" class="mz-header">
+    <!-- Logo -->
+    <div class="mz-header__logo">
+      <NuxtLink to="/">
+        <img
+          v-if="settings?.logo?.src"
+          :src="settings.logo.src"
+          :alt="settings.name || 'Site logo'"
+          class="h-10 w-auto"
+        />
+        <span v-else class="font-bold text-lg">{{ settings?.name || 'Site Name' }}</span>
+      </NuxtLink>
+    </div>
+    <!-- Navigation -->
     <nav class="mb-3">
       <ul class="mz-list mz-list--menu">
         <li
