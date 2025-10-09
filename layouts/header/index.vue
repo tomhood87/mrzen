2ee5a7e0f0f4a5a86ec61393674c12970361770e
Tom Hood <tom.hood@mrzen.com>
2025-10-09 15:51:11 +0100

    CSS updates.

--- layouts/header/index.vue
+++ layouts/header/index.vue
@@ -5,10 +5,11 @@ const { data:menu } = await useFetch('/api/menu')
 <template>
   <header id="header-layout-one" class="mz-header">
     <nav>
-      <ul>
+      <ul class="mz-list mz-list--menu">
         <li
           v-for="(item, i) in menu"
           :key="i"
+          class="mz-link"
         >
           <NuxtLink :to="`${item.slug}`">{{ item.title }}</NuxtLink>
         </li>
