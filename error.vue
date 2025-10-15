5e3b0d8d55f378a2f279689f681fe62eaf82c37f
Tom Hood <tom.hood@mrzen.com>
2025-10-15 15:37:52 +0100

    Add error page.

---
+++ error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="text-center">
+      <h1 class="text-2xl font-bold text-neutral-900 mb-2">Page not found</h1>
+      <p class="text-neutral-600">The requested page could not be loaded.</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
