6d140f6b19c0db47cc571ffc76548a7937b8a68e
Tom Hood <tom.hood@mrzen.com>
2025-10-17 12:47:16 +0100

    Try external import.

--- plugins/register-components.ts
+++
@@ -1,7 +0,0 @@
-import * as LayerComponents from '../components'
-
-export function registerLayerComponents(vueApp: any) {
-  Object.entries(LayerComponents).forEach(([name, component]) => {
-    vueApp.component(name, component)
-  })
-}
