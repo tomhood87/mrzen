0f35b4140f29fbd495dd63ac1e4d7fcb51e6bead
Tom Hood <tom.hood@mrzen.com>
2025-10-17 12:57:34 +0100

    Test header data as component.

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,10 +1,41 @@
 <script setup lang="ts">
-const { myLayer } = useAppConfig()
+const { data:menu } = await useFetch('/api/menu')
+const { data: settings } = await useAsyncData('site-settings', () =>
+  $fetch('/api/site-settings')
+)
+// Computed variables.
+//
+const filteredMenu = computed(() => {
+  const items = menu.value ? Object.values(menu.value) : []
+  return items.filter(item => item.slug !== '/')
+})
 </script>
 
 <template>
-  <div>
-    <h1>Hello World!</h1>
-    <pre>{{ myLayer }}</pre>
-  </div>
+  <header id="header-layout-one" class="mz-header">
+    <!-- Logo -->
+    <div class="mz-header__logo">
+      <NuxtLink to="/">
+        <img
+          v-if="settings?.logo?.src"
+          :src="settings.logo.src"
+          :alt="settings.name || 'Site logo'"
+          class="h-10 w-auto"
+        />
+        <span v-else class="font-bold text-lg">{{ settings?.name || 'Site Name' }}</span>
+      </NuxtLink>
+    </div>
+    <!-- Navigation -->
+    <nav class="d-none d-lg-block mb-3">
+      <ul class="mz-list mz-list--menu">
+        <li
+          v-for="(item, i) in filteredMenu"
+          :key="i"
+          class="mz-link"
+        >
+          <NuxtLink :to="`${item.slug}`">{{ item.title }}</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
 </template>
