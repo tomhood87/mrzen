e7f10ad44c3b5483050b7a667d72ffcdff5ec976
Tom Hood <tom.hood@mrzen.com>
2025-10-17 16:58:26 +0100

    Test property use.

--- components/HelloWorld.vue
+++ components/HelloWorld.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { useAppConfig } from '#app'
 const { myLayer } = useAppConfig()
+const props = defineProps<{
+  label?: string
+}>()
 </script>
 
 <template>
   <div>
-    <h1>Hello World!</h1>
+    <h1>Hello World! <span v-if="props.label">{{ props.label }}</span></h1>
     <pre>{{ myLayer }}</pre>
   </div>
 </template>
